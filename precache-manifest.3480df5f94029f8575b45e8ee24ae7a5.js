eab19d865839906881c05e7ce773f2c1b55c2303
ruby840124 <ruby840124@gmail.com>
2019-09-26 10:45:59 +0800

    Updates

---
+++ precache-manifest.3480df5f94029f8575b45e8ee24ae7a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7046d2df2300ba84eab947cbd655e2d",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "685d2ed95ccca12b97ea",
+    "url": "/repository-name/static/css/main.91b6c6b2.chunk.css"
+  },
+  {
+    "revision": "1eeb893a55bd805f65ca",
+    "url": "/repository-name/static/js/2.2114c912.chunk.js"
+  },
+  {
+    "revision": "685d2ed95ccca12b97ea",
+    "url": "/repository-name/static/js/main.b05eb38e.chunk.js"
+  },
+  {
+    "revision": "06ee32fabc18349fbc1e",
+    "url": "/repository-name/static/js/runtime-main.a651d538.js"
+  }
+]);
